cd269a619415f5e1deacd71c852e9c252637a86e
vidomarkas <vidomarkas@gmail.com>
2020-10-06 01:15:32 +0100

    add validation to input fields

---
+++ src/components/common/formControl/formControl.js
@@ -0,0 +1,40 @@
+import React from "react";
+import styles from "./formControl.module.scss";
+
+export const Textarea = (props) => {
+  const { input } = props;
+
+  return (
+    <div>
+      <textarea
+        {...input}
+        className={props.meta.touched && props.meta.error && styles.redBorder}
+      />
+      {props.meta.touched &&
+        ((props.meta.error && (
+          <span className={styles.error}>{props.meta.error}</span>
+        )) ||
+          (props.meta.warning && (
+            <span className={styles.warning}>{props.meta.warning}</span>
+          )))}
+    </div>
+  );
+};
+export const Input = ({ input, ...props }) => {
+  return (
+    <div>
+      <input
+        {...input}
+        {...props}
+        className={props.meta.touched && props.meta.error && styles.redBorder}
+      />
+      {props.meta.touched &&
+        ((props.meta.error && (
+          <span className={styles.error}>{props.meta.error}</span>
+        )) ||
+          (props.meta.warning && (
+            <span className={styles.warning}>{props.meta.warning}</span>
+          )))}
+    </div>
+  );
+};
