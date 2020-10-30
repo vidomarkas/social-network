4e06d710c858bef8c6ade35115e09a23fbfe7b98
vidomarkas <vidomarkas@gmail.com>
2020-10-30 00:31:57 +0000

    Updates

---
+++ precache-manifest.e6f2496c6b2e18844cd7a8760a73002d.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "199bc3c7ab8cc0d8b5e8bb4194d40da3",
+    "url": "/social-network/index.html"
+  },
+  {
+    "revision": "8af3de3cc67d64bee77b",
+    "url": "/social-network/static/css/3.a0825c20.chunk.css"
+  },
+  {
+    "revision": "12f08904fcf7263f2327",
+    "url": "/social-network/static/css/4.d81c4451.chunk.css"
+  },
+  {
+    "revision": "f4618d7406023753c4f9",
+    "url": "/social-network/static/css/main.d8b6f3a4.chunk.css"
+  },
+  {
+    "revision": "32c7f29c0bc7bbe1d71e",
+    "url": "/social-network/static/js/2.4fcce99d.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/social-network/static/js/2.4fcce99d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8af3de3cc67d64bee77b",
+    "url": "/social-network/static/js/3.e4ad5de2.chunk.js"
+  },
+  {
+    "revision": "12f08904fcf7263f2327",
+    "url": "/social-network/static/js/4.ed98d41b.chunk.js"
+  },
+  {
+    "revision": "f4618d7406023753c4f9",
+    "url": "/social-network/static/js/main.1df38f85.chunk.js"
+  },
+  {
+    "revision": "72f658ba2e175a783869",
+    "url": "/social-network/static/js/runtime-main.43025dcd.js"
+  },
+  {
+    "revision": "e8be04bac643acbdb9b01f5e0ea3790d",
+    "url": "/social-network/static/media/brands.e8be04ba.svg"
+  },
+  {
+    "revision": "32b9e2099065d99829fbba567e7c92a6",
+    "url": "/social-network/static/media/man.32b9e209.svg"
+  }
+]);
